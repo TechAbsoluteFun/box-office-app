5c812fbed1c9a057f9da154d6a1ea322fa574055
Mukunda <borah.mukunda96@gmail.com>
2024-01-28 19:21:44 +0530

    installed react-router-dom

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <div>
+      <Link to="/Starred">Go to Start Page</Link>
+    </div>
+  );
+};
+
+export default Home;
