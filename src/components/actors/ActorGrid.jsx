7c531f30bb67259cc1508942409f83676baac51c
Mukunda <borah.mukunda96@gmail.com>
2024-02-15 00:11:26 +0530

    styled the entire application using styled component

--- src/components/actors/ActorGrid.jsx
+++ src/components/actors/ActorGrid.jsx
@@ -1,8 +1,9 @@
 import ActorCard from './ActorCard';
+import { FlexGrid } from '../common/FlexGrid';
 
 const ActorGrid = ({ actors }) => {
   return (
-    <div>
+    <FlexGrid>
       {actors.map(data => (
         <ActorCard
           key={data.person.id}
@@ -18,7 +19,7 @@ const ActorGrid = ({ actors }) => {
           }
         />
       ))}
-    </div>
+    </FlexGrid>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import { searchForShows, searchForPeople } from './../api/tvmaze';
 import SearchForm from '../components/SearchForm';
 import ActorGrid from '../components/actors/ActorGrid';
 import ShowGrid from '../components/shows/ShowGrid';
+import { TextCenter } from '../components/common/TextCenter';
 
 const Home = () => {
   const [filter, setFilter] = useState(null);
@@ -25,11 +26,11 @@ const Home = () => {
 
   const renderApiData = () => {
     if (apiDataError) {
-      return <div>Error Occured: {apiDataError.message}</div>;
+      return <TextCenter>Error Occured: {apiDataError.message}</TextCenter>;
     }
 
     if (apiData?.length === 0) {
-      return <div>No Results</div>;
+      return <TextCenter>No Results</TextCenter>;
     }
 
     if (apiData) {
